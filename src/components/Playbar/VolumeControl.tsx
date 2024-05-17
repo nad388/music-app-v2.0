a3c712f38bd12f892f68c26e266ade55af84803a
Nadezhda Dmitrieva <n.dmitrieva388@gmail.com>
2024-05-17 22:36:45 +0300

    finished stylising volume control component

--- src/components/Playbar/VolumeControl.tsx
+++ src/components/Playbar/VolumeControl.tsx
@@ -1,12 +1,30 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import { BsVolumeDown, BsVolumeMute } from 'react-icons/bs'
 import { AudioContext } from '../../context/AudioContext'
 // import styles from './VolumeControl.module.scss'
 
-
 const VolumeControl = () => {
 	const { volume, handleVolumeChange } = useContext(AudioContext)
 	const isMuted = volume === 0 // Check if volume is muted (0)
+	const rangeRef = useRef<HTMLInputElement>(null)
+
+	useEffect(() => {
+		const range = rangeRef.current
+		if (!range) return // Проверка наличия элемента
+		const updateBackground = () => {
+			const min = Number(range.min)
+			const max = Number(range.max)
+			const currentVal = Number(range.value)
+			range.style.backgroundSize =
+				((currentVal - min) / (max - min)) * 100 + '% 100%'
+		}
+		updateBackground()
+		range.addEventListener('input', updateBackground)
+		return () => {
+			range.removeEventListener('input', updateBackground)
+		}
+	}, [])
+
 	return (
 		<div
 			style={{
@@ -19,7 +37,8 @@ const VolumeControl = () => {
 				{isMuted ? <BsVolumeMute size={30} /> : <BsVolumeDown size={30} />}
 			</div>
 			<input
-				// className={styles.inputRange}
+				ref={rangeRef}
+				className='range'
 				style={{ width: '100px' }}
 				type='range'
 				min='0'
